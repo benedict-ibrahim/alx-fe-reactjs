5208ebd44cecf43b28976ce1a9b1e65899e3d655
Benedict Ibrahim <benedictnomsuibrahim@gmail.com>
2025-07-25 22:54:35 +0100

    recipe app update

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import RecipeList from './components/RecipeList';
 import AddRecipeForm from './components/AddRecipeForm';
@@ -7,23 +7,25 @@ import EditRecipeForm from './components/EditRecipeForm';
 
 function App() {
   return (
-    <div className="app">
-      <header>
-        <h1>Recipe Sharing App</h1>
-      </header>
-      <main>
-        <Routes>
-          <Route path="/" element={
-            <>
-              <AddRecipeForm />
-              <RecipeList />
-            </>
-          } />
-          <Route path="/recipe/:id" element={<RecipeDetails />} />
-          <Route path="/edit/:id" element={<EditRecipeForm />} />
-        </Routes>
-      </main>
-    </div>
+    <Router>
+      <div className="app">
+        <header>
+          <h1>Recipe Sharing App</h1>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/" element={
+              <>
+                <AddRecipeForm />
+                <RecipeList />
+              </>
+            } />
+            <Route path="/recipe/:id" element={<RecipeDetails />} />
+            <Route path="/edit/:id" element={<EditRecipeForm />} />
+          </Routes>
+        </main>
+      </div>
+    </Router>
   );
 }
 
