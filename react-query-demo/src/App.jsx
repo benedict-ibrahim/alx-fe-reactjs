42b4a62328c172fb2fa12acc96632b4f7bc640d2
Benedict Ibrahim <benedictibraheem@gmail.com>
2025-08-19 10:36:32 +0100

    React Query

--- react-query-demo/src/App.jsx
+++ react-query-demo/src/App.jsx
@@ -1,12 +1,16 @@
-// src/App.jsx
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import PostsComponent from './components/PostsComponent';
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <div className="app">
-      <h1>React Query Demo</h1>
-      <PostsComponent />
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div className="app">
+        <h1>React Query Demo</h1>
+        <PostsComponent />
+      </div>
+    </QueryClientProvider>
   );
 }
 
