9c429e594182e9fdf80ec09d3c1c790824afebb7
Benedict Ibrahim <benedictibraheem@gmail.com>
2025-08-19 14:54:40 +0100

    react router

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,15 +1,40 @@
-import { Outlet } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Login from './pages/Login'
+import ProfileLayout from './pages/profile/ProfileLayout'
+import ProfileDetails from './pages/profile/ProfileDetails'
+import ProfileSettings from './pages/profile/ProfileSettings'
+import BlogPost from './pages/BlogPost'
+import ProtectedRoute from './components/ProtectedRoute'
 import Navigation from './components/Navigation'
 import './App.css'
 
 function App() {
   return (
-    <div className="app">
-      <Navigation />
-      <main className="main-content">
-        <Outlet />
-      </main>
-    </div>
+    <BrowserRouter>
+      <div className="app">
+        <Navigation />
+        <main className="main-content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/posts/:postId" element={<BlogPost />} />
+            
+            {/* Protected Routes */}
+            <Route path="/profile" element={
+              <ProtectedRoute>
+                <ProfileLayout />
+              </ProtectedRoute>
+            }>
+              <Route index element={<ProfileDetails />} />
+              <Route path="settings" element={<ProfileSettings />} />
+            </Route>
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
   )
 }
 
--- react-router-advanced/src/main.jsx
+++ react-router-advanced/src/main.jsx
@@ -1,51 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import { AuthProvider } from './context/AuthContext'
-import './index.css'
-
-// Import components
 import App from './App'
-import Home from './pages/Home'
-import About from './pages/About'
-import Login from './pages/Login'
-import ProfileLayout from './pages/profile/ProfileLayout'
-import ProfileDetails from './pages/profile/ProfileDetails'
-import ProfileSettings from './pages/profile/ProfileSettings'
-import BlogPost from './pages/BlogPost'
-import ProtectedRoute from './components/ProtectedRoute'
-import ErrorPage from './pages/ErrorPage'
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-    errorElement: <ErrorPage />,
-    children: [
-      { index: true, element: <Home /> },
-      { path: 'about', element: <About /> },
-      { path: 'login', element: <Login /> },
-      {
-        path: 'profile',
-        element: (
-          <ProtectedRoute>
-            <ProfileLayout />
-          </ProtectedRoute>
-        ),
-        children: [
-          { index: true, element: <ProfileDetails /> },
-          { path: 'settings', element: <ProfileSettings /> }
-        ]
-      },
-      { path: 'posts/:postId', element: <BlogPost /> }
-    ]
-  }
-])
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AuthProvider>
-      <RouterProvider router={router} />
+      <App />
     </AuthProvider>
   </React.StrictMode>
 )
